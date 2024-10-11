4f25f535e9803d5cd1b9009d910a1777bbeadaa2
sophia vincent <sophire33@gmail.com>
2024-10-11 08:45:41 -0700

    setting up kickzfitz homepage

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+
+const Hero = () => {
+  return (
+    <main className="hero">
+      <div className="hero-content">
+        <h1>The Best Balance For Your Feet</h1>
+        <p>
+            Step Up Your Shoe Game With Our Perfect
+            Feet Match. Step Up Your Shoe Game With Our Perfect
+            Feet Match.
+        </p>
+        <div className="hero-btn">
+            <button>Shop Now</button>
+            <button>Category</button>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src="/images/shoe.png" alt="shoe" />
+      </div>
+    </main>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div className='logo'>
+            <img src='/images/logo.jpg' alt='logo' className='logo'/>
+        </div>
+        <ul>
+            <li href="#">Menu</li>
+            <li href="#">Location</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+        </ul>
+
+        <button className='btn'>login</button>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
